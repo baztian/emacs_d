94ad8a680f5dd88fc1c1f65a4e6c5a3f0ad0aabc
baztian <bastian.dev@gmail.com>
2013-02-19 16:20:11 +0100

    unfill-paragraph

--- init.el
+++ init.el
@@ -150,6 +150,17 @@
   (goto-char (point-max))
   )
 
+;;; Stefan Monnier <foo at acm.org>. It is the opposite of fill-paragraph    
+(defun unfill-paragraph ()
+  "Takes a multi-line paragraph and makes it into a single line of text."
+  (interactive)
+  (let ((fill-column (point-max)))
+    (fill-paragraph nil)))
+
+;; Handy key definition
+(define-key global-map "\M-Q" 'unfill-paragraph)
+
+
 ;;;;;;;;;;;;
 ;;; Key defs
 ;;;;;;;;;;;;
